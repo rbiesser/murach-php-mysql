302e46afb2b34c261a6e8c21c5d28bc60e826f6c
Robbie Biesser <rbiesser@gmail.com>
2020-07-06 10:56:12 -0400

    u3gp1 close #18

--- ex_starts/ch19_ex1/index.php
+++ ex_starts/ch19_ex1/index.php
@@ -9,10 +9,14 @@ require('model/product_db.php');
  **********************************************/
 
 // Sample data
-$cat_id = 2;
+$cat_id = 1;
 
 // Get the products
 $products = get_products_by_category($cat_id);
+
+// it's weird that we are getting the products to display before doing the delete/add work.
+// that means that the product being displayed is actually a different productID than the 
+// one that was just inserted.
 
 /***************************************
  * Delete a product
@@ -22,7 +26,19 @@ $products = get_products_by_category($cat_id);
 $product_name = 'Fender Telecaster';
 
 // Delete the product and display an appropriate messge
-$delete_message = "No rows were deleted.";
+$product = get_product_by_name($product_name);
+if ($product) {
+    $product_id = $product['productID'];
+    $row_count = delete_product($product_id);
+    if ($row_count > 0) {
+        $delete_message = "$row_count row was deleted.";
+    } else {
+        $delete_message = "No rows were deleted.";
+    }
+} else {
+    $delete_message = "There is no product with that name.";
+}
+
 
 /***************************************
  * Insert a product
@@ -34,11 +50,19 @@ $code = 'tele';
 $name = 'Fender Telecaster';
 $description = 'NA';
 $price = '949.99';
+$discount_percent = 0;
 
 // Insert the data
+$product_id = add_product($category_id, $code, $name, $description,
+        $price, $discount_percent);
 
-// Display an appropriate message
-$insert_message = "No rows were inserted.";
+if ($product_id > 0) {
+    $insert_message = "1 row was inserted with Product ID: $product_id";
+} else {
+    // Display an appropriate message
+    $insert_message = "No rows were inserted.";
+}
+
 
 include 'home.php';
 ?>
--- ex_starts/ch19_ex1/util/main.php
+++ ex_starts/ch19_ex1/util/main.php
@@ -5,7 +5,10 @@ $doc_root = $_SERVER['DOCUMENT_ROOT'];
 // Get the application path
 $uri = $_SERVER['REQUEST_URI'];
 $dirs = explode('/', $uri);
-$app_path = '/' . $dirs[1] . '/' . $dirs[2] . '/';
+
+// fails if app is hosted from localhost without nested directories
+// $app_path = '/' . $dirs[1] . '/' . $dirs[2] . '/';
+$app_path = '/';
 
 // Set the include path
 set_include_path($doc_root . $app_path);
